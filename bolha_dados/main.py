bd9b20e44da294a6fd0da30bc99ac5645686a5d7
Breno Fernandes <brenojesusfernandes@pm.com>
2022-05-23 21:03:15 -0230

    Fix: Heroku deploy

--- bolha_dados/main.py
+++ bolha_dados/main.py
@@ -4,6 +4,13 @@ from dataclasses import dataclass
 from typing import List
 import os
 import asyncio
+import time
+import urllib.parse
+
+import sys
+from subprocess import Popen, PIPE
+
+p = Popen([sys.executable, "-m", "playwright", "install"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
 
 @dataclass
@@ -20,7 +27,8 @@ class TwitterBot:
     browser_visible: bool = True
 
     async def __browser_init(self) -> Page:
-        browser = await self.client.chromium.launch(headless=not self.browser_visible)
+        browser = await self.client.chromium.launch(chromium_sandbox=False,
+                                                    headless=not self.browser_visible)
 
         context = await browser.new_context(storage_state=self.cookie_path)
         page = await context.new_page()
@@ -36,20 +44,20 @@ class TwitterBot:
         return page
 
     async def __is_tweets(self, page: Page) -> bool:
-
-        if page.locator(self.__get_tweets_xpath).count() == 0:
+        tot = await page.locator(self.__get_tweets_xpath).count()
+        if tot == 0:
             logger.info("Hmmm... There's no new tweet to interact.")
             return False
 
         return True
 
     async def __is_not_interacted_tweets(self, page: Locator) -> bool:
-
-        if page.locator(self.__get_tweets_xpath).count() == 0:
+        tot = await page.locator(self.__get_tweets_xpath).count()
+        if tot == 0:
             logger.info("Hmmm... There's no new tweet to interact.")
-            return False
+            return True
 
-        return True
+        return False
 
     @property
     def __get_tweets_xpath(self) -> str:
@@ -59,7 +67,7 @@ class TwitterBot:
 
     async def __interact_tweets(self, page: Page, tweets_locator: Locator, actions: List[str]):
 
-        logger.info(f'Amount Tweets Founded: "{tweets_locator.count()}"')
+        logger.info(f'Amount Tweets Founded: "{await tweets_locator.count()}"')
 
         if await self.__is_not_interacted_tweets(tweets_locator):
             tweets_id: List[str] = await self.__find_tweets_id(tweets_locator)
@@ -71,10 +79,11 @@ class TwitterBot:
                 for index in range(await event_elements.count()):
                     event_element = event_elements.nth(index)
 
-                    if event_element.get_attribute('data-testid') == 'like':
+                    data_testid = await event_element.get_attribute('data-testid')
+                    if data_testid == 'like':
                         await event_element.click()
                         break
-                    elif event_element.get_attribute('data-testid') == 'unlike':
+                    elif data_testid == 'unlike':
                         logger.info('There is not new tweet!!!')
                         is_new_tweet = False
 
@@ -122,9 +131,9 @@ async def main() -> None:
     async with async_playwright() as play_wright:
         bolha_dados_bot = TwitterBot(client=play_wright,
                                      cookie_path=os.getenv('COOKIE_PATH'),
-                                     query=os.getenv('TWITTER_QUERY'),
-                                     src_option=os.getenv('SRC_OPTION'),
-                                     from_option=os.getenv('FROM_OPTION'),
+                                     query=urllib.parse.quote(os.getenv('TWITTER_QUERY')),
+                                     src_option=urllib.parse.quote(os.getenv('SRC_OPTION')),
+                                     from_option=urllib.parse.quote(os.getenv('FROM_OPTION')),
                                      default_wait_time_ms=int(os.getenv('DEFAULT_TIME_WAIT_MS')),
                                      browser_visible=bool(os.getenv('BROWSER_VISIBLE')))
         await bolha_dados_bot.run()
